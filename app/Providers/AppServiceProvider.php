9ba13e8023eef55c67963f830a4e586f3b4cf4fe
Daniel Chung <danielvchung@hotmail.com>
2020-01-10 12:47:55 -0300

    more events

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Mail\UserRegistered;
 use App\User;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\ServiceProvider;
 
@@ -27,8 +28,27 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // Evento = Ação e Disparo
+        User::creating(function ($user) {
+            Log::info("User {$user->email}, event CREATING.");
+        });
+        User::creating(function ($user) {
+            Log::info("User {$user->email}, event CREATING - 2.");
+        });
         User::created(function ($user) {
-            Mail::to($user)->send(new UserRegistered($user));
+            // Mail::to($user)->send(new UserRegistered($user));
+            Log::info("User {$user->email}, event CREATED.");
+        });
+        User::updating(function ($user) {
+            Log::info("User {$user->email}, event UPDATING.");
+        });
+        User::updated(function ($user) {
+            Log::info("User {$user->email}, event UPDATED.");
+        });
+        User::saving(function ($user) {
+            Log::info("User {$user->email}, event SAVING.");
+        });
+        User::saved(function ($user) {
+            Log::info("User {$user->email}, event SAVED.");
         });
     }
 }
